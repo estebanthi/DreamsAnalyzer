52a69e12c6fd97398e6282a7e0485461d51cef79
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-05-27 12:10:50 +0200

    refacto

---
+++ src/models/bbcode/bbcode_tags.py
@@ -0,0 +1,19 @@
+from src.models.bbcode.bbcode_tag import BBCodeTag
+
+
+class BBCodeColorTag(BBCodeTag):
+
+    def __init__(self, color):
+        super().__init__('color', color)
+
+
+class BBCodeItalicTag(BBCodeTag):
+
+    def __init__(self):
+        super().__init__('i')
+
+
+class BBCodeBoldTag(BBCodeTag):
+
+    def __init__(self):
+        super().__init__('b')
